e70d28df10d55496fc6ba204018540277958b9cc
triyono <yono.tri@gmail.com>
2025-03-21 18:22:35 +0700

    fix session login

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'pages/auth/login_page.dart';
+import 'pages/home/home_page.dart';
 
 void main() async {
-  await GetStorage.init();
+  await GetStorage.init(); // Inisialisasi GetStorage
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final _storage = GetStorage(); // Instance GetStorage
+
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    // Cek apakah user masih login
+    final isLoggedIn = _storage.read('user') != null;
+
+    return MaterialApp(
       title: 'Flutter GetStorage App',
-      home: LoginPage(),
+      // Jika user masih login, arahkan ke HomePage, jika tidak, arahkan ke LoginPage
+      home: isLoggedIn ? HomePage() : LoginPage(),
     );
   }
 }
